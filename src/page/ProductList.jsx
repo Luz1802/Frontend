1e284c9af9921af50d8b41fc49981788d000a4ab
Luz1802 <lbonillama@cesde.net>
2026-02-06 20:49:23 -0500

    ProductList + Css

--- src/page/ProductList.jsx
+++ src/page/ProductList.jsx
@@ -1,1 +1,30 @@
-function ProductList() {}
+import { products } from '../data/products';
+import ProductCard from '../components/ProductCard';
+import styles from './ProductList.module.css';
+
+function ProductList() {
+
+    return (
+        <div className = {styles.container}>
+            <header className = {styles.header}>
+            <h1 className = {styles.title}>Lista de Productos</h1>
+            <p className = {styles.description}>Explora nuestra selección de productos!</p>
+            </header>
+
+            <div className = {styles.grid}>
+            {products.map(product => (
+                <ProductCard
+                key={product.id}
+                name={product.name}
+                category={product.category}
+                price={product.price}
+                image={product.image}
+                description={product.description}
+                />
+            ))}
+            </div>
+        </div>
+    )           
+}
+
+
