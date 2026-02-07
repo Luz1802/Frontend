f79280268bc813900132eca582db202d5930f296
Luz1802 <lbonillama@cesde.net>
2026-02-06 20:19:01 -0500

    ProductsCard1

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,2 +1,18 @@
-function ProductCard () {
+function ProductCard ( name, price, description, image, category ) { 
+
+    return (
+
+        <article className = "product-card">
+            <img src = { image } alt = { name } className = "product-image" />
+            <div className = "product-info">
+                <span className = "product-category">{ category }</span>
+                <h3 className = "product-name">{ name }</h3>
+                <p className = "product-description">{ description }</p>
+                <div className = "product-foooter">
+                    <span className = "product-price">${ price.toFixed(2) }</span>
+                    <button className = "btn-like">Me gusta</button>
+                </div>
+            </div>
+        </article>
+    )
 }
