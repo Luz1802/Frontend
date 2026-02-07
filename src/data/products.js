4a68a0bbf077ec70a23bd816cfadb9bd792ca865
Luz1802 <lbonillama@cesde.net>
2026-02-06 20:05:33 -0500

    4 productos

---
+++ src/data/products.js
@@ -0,0 +1,37 @@
+export const products = [ 
+    {
+        id: 1,
+        name: "Laptop HP",
+        category: "Laptops",
+        price: 999.99,
+        image: "laptop_hp.jpg",
+        description: "Laptop potente con procesador Core i7, 168GB RAM y 512GB SSD.",
+    },
+
+    {
+        id: 2,
+        name: "Smartphone Samsung",
+        category: "Smartphones",
+        price: 699.99,
+        image: "smartphone_samsung.jpg",
+        description: "Smartphone Samsung con pantalla AMOLED de 6.7 pulgadas y cámara de 50MP.",
+    },
+
+    {
+        id: 3,
+        name: "Tablet Apple iPad",
+        category: "Tablets",
+        price: 499.99,
+        image: "tablet_ipad.jpg",
+        description: "iPad con pantalla Retina de 10.2 pulgadas, chip A13 Bionic y 64GB de almacenamiento.",
+    },
+
+    {
+        id: 4,
+        name: "Auriculares Sony WH-1000XM4",
+        category: "Audio",  
+        price: 299.99,
+        image: "auriculares_sony.jpg",
+        description: "Auriculares inalámbricos con cancelación de ruido y calidad de sonido premium.",
+    }
+];  
