5ce828ec865cbe07882c89c1d799763cda2da9c2
Luz1802 <lbonillama@cesde.net>
2026-02-20 21:01:39 -0500

    Paso 2

--- src/page/ProductList.jsx
+++ src/page/ProductList.jsx
@@ -1,33 +1,36 @@
-import { products } from '../data/products';
+import { useState } from "react";
+import { products } from '../data/Product';
 import ProductCard from '../components/ProductCard';
 import styles from './ProductList.module.css';
+import ProductForm from "../components/ProductForm";
 
 function ProductList() {
+    const [productsState, setProductsState] = useState(products);
 
     return (
-        <div className = {styles.container}>
-            <header className = {styles.header}>
-            <h1 className = {styles.title}>Lista de Productos</h1>
-            <p className = {styles.description}>Explora nuestra selección de productos!</p>
+        <div className={styles.container}>
+            <header className={styles.header}>
+                <h1 className={styles.title}>Productos informaticos</h1>
+                <p className={styles.subtitle}>
+                    Encuentra los mejores productos de tecnologia pra tu setup
+                </p>
             </header>
 
-            <div className = {styles.grid}>
-            {products.map(product => (
-                <ProductCard
-                key={product.id}
-                name={product.name}
-                category={product.category}
-                price={product.price}
-                stock={product.stock}
-                image={product.image}
-                description={product.description}
-                />
-            ))}
+            <div className={styles.grid}>
+                {productsState.map((product) => (
+                    <ProductCard
+                        key={product.id}
+                        name={product.name}
+                        category={product.category}
+                        price={product.price}
+                        stock={product.stock}
+                        image={product.image}
+                        description={product.description}
+                    />
+                ))}
             </div>
         </div>
-    )           
+    );
 }
 
 export default ProductList;
-
-
